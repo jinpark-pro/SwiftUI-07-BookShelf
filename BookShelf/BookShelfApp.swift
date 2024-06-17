9da48ee81d339e82a187feccd6365ae5b9e1c42a
Jin Park <jinpark1504@gmail.com>
2024-06-17 10:09:11 +0900

    Initial Commit

---
+++ BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by Jungjin Park on 2024-06-17.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
